c92650c98cadb644da22a917465c6ea5ddf8eded
NIDHINgl <abeynidhin.g.l@gmail.com>
2026-01-03 22:20:33 +0530

    Updates

--- _next/static/XNd9uOfgdQVsz53SWkroR/_buildManifest.js
+++ _next/static/XNd9uOfgdQVsz53SWkroR/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Fin-Plan//_next/:path+",
+        "destination": "/Fin-Plan/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
